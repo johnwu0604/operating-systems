3fe3ec7899d817f2c92084e81bde62fd3ad17ace
John Wu <john.wu@mail.mcgill.ca>
2018-02-03 23:08:00 -0500

    ff

--- A1/a1_shell.c
+++ A1/a1_shell.c
@@ -495,6 +495,7 @@ int main(void)
                 {
                     //simply execute the command.
                     execvp(args[0], args);
+                    waitpid(pid, &status, 0);
                 }
             }
         }
