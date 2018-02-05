34bcd971d892d894ee780118537d4c0557c495f8
John Wu <john.wu@mail.mcgill.ca>
2018-02-04 21:29:44 -0500

    ff

--- A1/a1_shell.c
+++ A1/a1_shell.c
@@ -491,8 +491,7 @@ int main(void)
                     //open file and change output from stdout to that
                     //make sure you use all the read write exec authorisation flags
                     //while you use open (man 2 open) to open file
-                    char *token;
-                    token = strtok(args[0], "<");
+                    char *token = strtok(args[0], ">");
                     printf("%s", token);
 
 //                    //set ">" and redirected filename to NULL
